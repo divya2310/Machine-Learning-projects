07b54526ad804cec85c7e9f6d5964446bef1eae7
divya2310 <divyakamra2310@gmail.com>
2024-04-07 20:32:29 +0530

    This is the commit for web application

--- app.py
+++ app.py
@@ -1,53 +1,28 @@
-import streamlit as st
-
 import pickle
+from flask import Flask, request,app,jsonify,url_for,render_template
 
-## importing the model
-pipe = pickle.load(open('pipe.pkl','rb'))
-df = pickle.load(open('df.pkl','rb') ,'UTF-8')
+import numpy as np 
+import pandas as pd 
 
-st.title("Laptop Predictor")
+app=Flask(__name__)
+## Load the model
+regmodel=pickle.load(open('regmodel.pkl','rb'))
+scalar=pickle.load(open('scaling.pkl','rb'))
 
-## collecting info from the user
+@app.route('/')
+def home():
+    return render_template('home.html')
 
-## brand
-company = st.selectbox('Brand',df['Company'].unique())
-
-## type of laptop
-type = st.selectbox('Type',df['TypeName'].unique())
-
-## ram
-Ram = st.selectbox('Ram(in GB)',[2,4,6,8,12,16,24,32,64])
-
-
-## tocuhscreen
-Touchscreen = st.selectbox('Touchscreen',['No','Yes'])
-
-## IPS
-IPS = st.selectbox('IPS',['No','Yes'])
-
-## screen_size
-screen_size = st.number_input('Screen Size')
-
-## resolution
-resolution = st.selectbox('Screen Resolution',['1920*1080','1366*768',
-                                               '1600*900','3840*1800','2880*1800',
-                                               '2560*1600'])
-
-## cpu brand
-
-CPU = st.selectbox('Brand',df['Cpu Brand'].unique())
-
-HDD =  st.selectbox('HDD(in GB)',['0','128','256','512','1024','2048'])
-
-SSD  = st.selectbox('SSD(in GB)',['0','128','256','512','1024'])
-
-GPU = st.selectbox('Brand',df['Brand Name'].unique())
-OS = st.selectbox('OS',df['OS'].unique())
-
-if st.button('Predict Price'):
-    pass
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    data=request.json['data']
+    print(np.array(list(data.values())).reshape(1,-1))
+    new_data=scalar.transform(np.array(list(data.values())).reshape(1,-1))
+    output=regmodel.predict(new_data)
+    print(output[0])
+    return jsonify(output[0])
+    
 
 
-
-
+if __name__=="__main__":
+    app.run(debug=True)
